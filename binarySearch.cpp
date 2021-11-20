8954a88171f37ec692da94448dc004acfdc0d815
Yudi Krisnandi <yudikrisnandi22@gmail.com>
2021-11-20 16:04:55 +0700

    binary search

---
+++ binarySearch.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h> 
+
+using namespace std;
+
+int binary_search(int arr[], int n, int target){
+  int s = 0;
+  int e = n - 1;
+  while(s <= e){
+    int mid = (s+e) / 2;
+    if(arr[mid] == target){
+      return mid;
+    }else if(arr[mid] > target){
+      e = mid - 1;
+    }else{
+      s = mid + 1;
+    }
+  }
+  return -1;
+}
+ 
+int main(){
+  int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  int size = sizeof(arr) / sizeof(int);
+
+  int target; cin >> target;
+
+  int index = binary_search(arr, size, target);
+
+  cout << index << endl;
+
+  return 0;
+}
