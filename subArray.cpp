dc904d05a8c64bf0efbe05b6a09de10a24e483e8
Yudi Krisnandi <yudikrisnandi22@gmail.com>
2021-11-22 07:00:57 +0700

    sub array

---
+++ subArray.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h> 
+
+using namespace std;
+
+void print_subarray(int arr[], int n ){
+  for(int i = 0; i < n; i++){
+    for(int j = i; j < n; j++){
+      //cout << "(" << i << "," << j << "),";
+      for(int k=i; k <= j; k++){
+        cout << arr[k] << ", ";
+      }
+      cout << endl;
+    }
+  }
+}
+ 
+int main(){
+  int arr[] = {1, 2, 3, 4, 5, 6};
+
+  int size = sizeof(arr) / sizeof(int);
+
+  print_subarray(arr, size);
+
+  return 0;
+}
