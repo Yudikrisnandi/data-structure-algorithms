270c29ebbd5de70106c24f02432dfa6618b41cac
Yudi Krisnandi <yudikrisnandi22@gmail.com>
2021-12-02 08:07:53 +0700

    pointer

---
+++ pointer.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h> 
+
+using namespace std;
+
+ 
+int main(){
+  int x = 10;
+  cout << "print address of x " << &x << endl;
+  // pointer variable 
+  int *p = &x;
+  cout << "print value pointer variable p " << p << endl;
+  cout << "dereference operator " << *p << endl;
+  cout << "print address of p " << &p << endl;
+  // pointer to pointer
+  int **pp = &p;
+  cout << "print value of pointer to pointer pp " << pp << endl;
+  cout << "print address of pointer to pointer pp " << &pp << endl;
+  int *zeroptr = 0;
+  int *nulptr = NULL;
+  cout << zeroptr << endl;
+  cout << nulptr << endl;
+
+  //reference variable
+  int a = 10;
+  int b = 10; // a and b different same bucket
+  int &c = a; // a and c same bucket
+  a++;
+  cout << a << " " << c << endl;
+
+  return 0;
+}
