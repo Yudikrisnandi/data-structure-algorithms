40b5263d50e6860045d10b9f7f62ca97d95fa8ce
Yudi Krisnandi <yudikrisnandi22@gmail.com>
2022-03-30 08:56:26 +0700

    buble sort

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Referensi untuk belajar basic data structure & algoritm
+# Note
 
 ### Cara compile program c++
 
@@ -18,7 +18,7 @@ g++ -o outputfilename filename.c++
 
 [source](https://en.wikipedia.org/wiki/Bit_numbering#:~:text=In%20computing%2C%20the%20least%20significant,place%20of%20the%20binary%20integer.)
 
-#### Binary operato
+#### Binary operator
  - Binary AND &
  - Binary OR |
  - Binary XOR ^
--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -12,17 +12,37 @@ void bubbleSort(int a[], int n ){
   }  
 }
 
+void bubbleSort2(int a[], int n ){
+  bool swapped;
+  do {
+    swapped = false;
+    for(int i = 0; i < n; i++){
+      if(a[i] > a[i+1]){
+        swap(a[i], a[i+1]);
+        swapped = true;
+      }
+    }
+  }while(swapped);
+}
+
 
 int main(){
   int arr[] = {-2, 3, 4, -1, 5, -12, 6, 1, 3};
+  int arr2[] = {-4, 5, 4, -1, 5, -12, 8, 1, 3};
 
   int size = sizeof(arr) / sizeof(int);
 
   bubbleSort(arr, size);
+  bubbleSort2(arr2, size);
 
   for(auto x : arr){
     cout << x << ", ";
   }
 
+  cout << endl;
+
+  for(auto x : arr2){
+    cout << x << ", ";
+  }
   return 0;
 }
